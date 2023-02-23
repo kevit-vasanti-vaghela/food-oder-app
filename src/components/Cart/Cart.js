bcf2e68f958b67d8eeb2ec31092a0d8a7f762ddc
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-02-23 18:42:38 +0530

    bind is used to preconfigure function

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,28 @@ import React, { useContext } from 'react'
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css'
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
     const cartCtx = useContext(CartContext)
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
     const hasItems = cartCtx.items.length > 0
 
+    const cartItemRemoveHandler = id => {}
+    const cartItemAddHandler = item => {}
+
     const cartItems = <ul className={classes['cart-items']}>
         {
-            cartCtx.items.map((item) => <li>{item.name}</li>)
+            cartCtx.items.map((item) => (
+              <CartItem 
+                key={item.id} 
+                name={item.name} 
+                amount={item.amount} 
+                price={item.price} 
+                onRemove={cartItemRemoveHandler.bind(null, item.id)}  //Bind allows to preconfigure the function for future execution...here we preconfigure argument that function will get when get called.
+                onAdd={cartItemAddHandler.bind(null, item)}
+              />
+            ))
         }
     </ul>;
     
