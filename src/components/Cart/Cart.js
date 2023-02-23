be25ea4ff351e56f14ea2815c1b03758c39ea07b
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-02-23 17:16:28 +0530

    total amount got from cart context and displayed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css'
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext)
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
     const cartItems = <ul className={classes['cart-items']}>
         {
-            [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 },].map((item) => <li>{item.name}</li>)
+            cartCtx.items.map((item) => <li>{item.name}</li>)
         }
     </ul>;
     
@@ -14,7 +17,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose} >Close</button>
