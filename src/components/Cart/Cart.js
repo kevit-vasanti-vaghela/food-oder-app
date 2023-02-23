aa426cd5a3158112552e523390e0b18bc8d5ca77
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-02-23 10:29:09 +0530

    form and cart componenet created

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './Cart.module.css'
 
 const Cart = (props) => {
-    const cartItems = <ul>
+    const cartItems = <ul className={classes['cart-items']}>
         {
             [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 },].map((item) => <li>{item.name}</li>)
         }
@@ -11,7 +11,16 @@ const Cart = (props) => {
   return (
     <div>
       {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
     </div>
+    
   )
 }
 
