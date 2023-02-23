3d4853a5d657f5f2656463f13ebf90044df6e67b
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-02-23 17:39:23 +0530

    order button conditionally rendered

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,6 +6,8 @@ import CartContext from '../../store/cart-context';
 const Cart = (props) => {
     const cartCtx = useContext(CartContext)
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+    const hasItems = cartCtx.items.length > 0
+
     const cartItems = <ul className={classes['cart-items']}>
         {
             cartCtx.items.map((item) => <li>{item.name}</li>)
@@ -21,7 +23,7 @@ const Cart = (props) => {
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose} >Close</button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
     
